2d93195d7bd7ca00649a51981ce3a0ecd20fbc78
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-25 23:36:51 -0500

    wrap with context provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,26 @@
 
 import Image from "next/image";
 import ShellNav from "@/components/ShellNav/ShellNav";
+import BashLanding from "@/components/Landing/Landing";
+
+import { BashProvider } from "@/context/BashContext";
+import { useBashContext } from "@/context/BashContext";
 
 
 {/* <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold"></code> */}
 
+
 export default function Home() {
+  
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center !h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
-      <ShellNav placeholder="chmod +x bash-a-crumb.sh && ./bash-a-crumb.sh" />
 
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left">
-          <li className="mb-2">
-            Execute the command above!
-          </li>
-          <li>Navigate!</li>
-        </ol>
+
+        <BashProvider>
+          <BashLanding />
+        </BashProvider>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
 
