79c5e221e0c952a18339d3d0a51b32ecd21c705b
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-25 21:20:23 -0500

    gut rest of boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,22 @@
 "use client";
 
 import Image from "next/image";
-
 import ShellNav from "@/components/ShellNav/ShellNav";
+
+
+{/* <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold"></code> */}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <ShellNav />
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center !h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
+      <ShellNav placeholder="chmod +x bash-a-crumb.sh && ./bash-a-crumb.sh" />
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            Execute the command above!
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Navigate!</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
