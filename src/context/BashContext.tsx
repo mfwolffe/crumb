4572408c81917dcdc02f8f1855759542cc3465a1
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-25 22:10:19 -0500

    flesh out context, providers, etc

--- src/context/BashContext.tsx
+++ src/context/BashContext.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+
+// TODO @mfwolffe fix types stuff
+//
+
 import React, { createContext, useReducer, useContext } from 'react';
 
 
@@ -11,3 +15,29 @@ const baseState = {
   ps1hColorClass: "text-teal-300",
 }
 
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:                    return state;
+    case "START":               return { ...state, executed: true };
+    case "UPDATE_UNAME":        return { ...state, userName: action.payload };
+    case "UPDATE_HOSTNAME":     return { ...state, hostName: action.payload };
+    case "UPDATE_PS1_UNAME_C":  return { ...state, ps1uColorClass: action.payload };
+    case "UPDATE_PS1_HNAME_C":  return { ...state, ps1hColorClass: action.payload };
+  }
+}
+
+const BashContext = createContext();
+
+
+export const BashProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, baseState);
+
+  return (
+    <BashContext.Provider value={{ state, dispatch }}>
+      { children }
+    </BashContext.Provider>
+  )
+}
+
+export const useBashContext = () => useContext(BashContext);
