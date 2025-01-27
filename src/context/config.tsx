ad1a339c3aa6160eebdfab1fec266ffa4d23bddb
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-26 19:30:40 -0500

    just tw classes

--- src/context/config.tsx
+++ src/context/config.tsx
@@ -5,8 +5,8 @@ const PS1 = () => {
   const { state } = useBashContext();
 
   return (
-    <span className="inline-block px-3 py-2 text-sm font-medium text-gray-700 bg-gray-200 dark:bg-gray-700 dark:text-gray-300 rounded-l-md">
-      <span className={state.ps1uColorClass}>{ state.userName }</span>@<span className={state.ps1hColorClass}>{ state.hostName }</span>:~$
+    <span className="inline-block px-3 py-2 text-sm font-medium bg-gray-200 dark:bg-gray-700 rounded-l-md">
+      <span className={ state.ps1uColorClass }>{ state.userName }</span>@<span className={ state.ps1hColorClass }>{ state.hostName }</span>:~$
     </span>
   )
 }
